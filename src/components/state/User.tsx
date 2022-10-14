ea78b12d8aa4f690def9f895ef1f54ecd2b329f4
Anthonie Lorsithong <anthoniecorps@gmail.com>
2022-10-13 21:15:07 -0700

    DSA

---
+++ src/components/state/User.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+type AuthUser = {
+  name: string
+  email: string
+}
+
+
+export const User = () => {
+  const [user, setUser] = useState<AuthUser | null>(null)
+  // const [user, setUser] = useState<AuthUser>({} as AuthUser) ====== Type Assertion 
+
+  const handleLogin = () => {
+    setUser({
+      name: 'Anthonie', 
+      email: 'anthonie56@gmail.com'
+    })
+  }
+  const handleLogout = () => {
+    setUser(null)
+  }
+
+  return (
+    <div>
+      <button onClick={ handleLogin }>Login</button>
+      <button onClick={ handleLogout }>Logout</button>
+      <div>User name is {user?.name}</div>
+      <div>User email is {user?.email}</div>
+    </div>
+  )
+}
